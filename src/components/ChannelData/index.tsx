b565dafc229ef1c56b334caf70332db37ea31ebf
kassio Aguirres <kassio1a2b3@hotmail.com>
2020-09-05 20:57:39 -0300

    first commit

---
+++ src/components/ChannelData/index.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect } from 'react';
+
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
+import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
+
+const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef]);
+
+  return (
+    <Container>
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            key={n}
+            author="Aguirres K."
+            date="05/09/2020"
+            content="Criando interface do discord"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Tiago Araujo"
+          date="05/09/2020"
+          content={
+            <>
+              <Mention>@Aguirres K.</Mention> Bora jogar um Mobile legends?
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
+
+      <InputWrapper>
+        <Input type="text" placeholder="Conversarem #chat-livre" />
+        <InputIcon />
+      </InputWrapper>
+    </Container>
+  );
+};
+
+export default ChannelData;
---
+++ src/components/ServerButton/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Recket from '../../assets/Logo.svg';
+
+import { Button } from './styles';
+
+export interface Props {
+  selected?: boolean;
+  isHome?: boolean;
+  hasNotifications?: boolean;
+  mentions?: number;
+}
+
+const ServerButton: React.FC<Props> = ({
+  selected,
+  isHome,
+  hasNotifications,
+  mentions,
+}) => {
+  return (
+    <Button
+      isHome={isHome}
+      hasNotifications={hasNotifications}
+      mentions={mentions}
+      className={selected ? 'active' : ''}
+    >
+      {isHome && <img src={Recket} alt="Rocketseat" />}
+    </Button>
+  );
+};
+
+export default ServerButton;
